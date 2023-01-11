5460afb5d3c76009bc176d8a7cd1e20f277e89c9
Noah-Harris1 <117109605+Noah-Harris1@users.noreply.github.com>
2023-01-11 10:55:53 -0500

    Variable Name Bug Fix

--- FINAL_BUILD/FINAL_CLIENT/src/index.jsx
+++ FINAL_BUILD/FINAL_CLIENT/src/index.jsx
@@ -1,14 +1,14 @@
-import { Provider } from 'react-redux';
-import { Store } from "./Store/Store";
+
 import ReactDOM from 'react-dom';
-import { App } from './App';
 import React from 'react';
+
+import { App } from './App';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render (
   <React.StrictMode>
-    <Provider store={Store}>
-      <App />
-		</Provider>
+    <App />
   </React.StrictMode>,
 	
   document.getElementById('root')
